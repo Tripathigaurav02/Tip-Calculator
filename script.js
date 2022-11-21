411d616b7469a72789d333e06eb6d3e3a10e0db1
Gaurav Kumar Tripathi <83753529+Tripathigaurav02@users.noreply.github.com>
2022-11-21 23:39:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+// Get number of people from number of people div
+let numberOfPeople = Number(numberOfPeopleDiv.innerText)
+
+// ** Calculate the total bill per person **
+const calculateBill = () => {
+  // get bill from user input & convert it into a number
+const bill = Number(billInput.value)
+
+
+  // get the tip from user & convert it into a percentage (divide by 100)
+  const tipPercentage = Number(tipInput.value)/100
+
+  // get the total tip amount
+  let totalTipAmount = bill + (bill*tipPercentage)
+  
+
+  // calculate the per person total (total divided by number of people)
+let perPersonTotal = totalTipAmount/numberOfPeople
+
+  // update the perPersonTotal on DOM & show it to user
+perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
+}
+
+// ** Splits the bill between more people **
+const increasePeople = () => {
+  // increment the amount of people
+numberOfPeople +=1;
+
+  // update the DOM with the new number of people
+numberOfPeopleDiv.innerText = numberOfPeople
+
+  // calculate the bill based on the new number of people
+calculateBill()
+}
+
+// ** Splits the bill between fewer people **
+const decreasePeople = () => {
+  // guard clause
+  // if amount is 1 or less simply return
+  // (a.k.a you can't decrease the number of people to 0 or negative!)
+if(numberOfPeople<=1){
+  throw 'You cannot have less tan 1 Person'
+  return
+}
+  
+  // decrement the amount of people
+numberOfPeople -=1;
+
+  // update the DOM with the new number of people
+numberOfPeopleDiv.innerText = numberOfPeople
+
+  // calculate the bill based on the new number of people
+
+calculateBill()
+  
+}
